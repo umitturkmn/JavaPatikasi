b3b1aa54299db2a408ccfd3c2c88b781232cdb77
umitturkmn <umit.482@gmail.com>
2022-04-07 18:16:42 +0300

    küçük bir değişiklik yapıldı

--- src/KDVHesaplama.java
+++ src/KDVHesaplama.java
@@ -4,11 +4,14 @@ public class KDVHesaplama {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double fiyat,vergisi,vergili;
+        boolean buyuk,kucuk;
         System.out.println("Katma Değer Vergisi Hesaplama Programı...");
         System.out.print("-----------------------------------------\nKDV yüzde 18 olarak hesaplanacaktır.\n");
         System.out.print("Lütfen hesaplamak istediğiniz ürünün Fiyatını giriniz:");
         fiyat = input.nextDouble();
-        vergisi = (fiyat*18)/100;
+        buyuk = fiyat >= 1000;
+        kucuk = fiyat >= 0;
+        vergisi = buyuk && kucuk ? (fiyat*8)/100 : (fiyat*18)/100;
         vergili = fiyat + vergisi;
         System.out.println("KDV'siz Fiyatı: " + fiyat);
         System.out.println("KDV tutarı: " + vergisi);
