95e9ae3ccf416c49886a7fca61adc2da479b8500
umitturkmn <umit.482@gmail.com>
2022-04-08 07:23:50 +0300

    Hesap Makinası tamamlandı.

---
+++ src/HesapMakinası.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class HesapMakinası {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a, b, toplama, cikarma, bolme, carpma;
+        int secim;
+        System.out.println("******************************");
+        System.out.println("*******-HESAP MAKİNASI-*******");
+        System.out.println("******************************");
+        System.out.print("1. Sayıyı Giriniz: ");
+        a = input.nextDouble();
+        System.out.print("2. Sayıyı Giriniz: ");
+        b = input.nextDouble();
+        System.out.println("Yapmak istediğiniz işlemi seçin.\nToplama(1)\nÇıkarma(2)\nÇarpma(3)\nBölme(4)");
+        System.out.print("Seçiminiz: ");
+        secim = input.nextInt();
+
+
+        switch (secim) {
+            case 1:
+                toplama = a + b;
+                System.out.println("SONUÇ: " + toplama);
+                break;
+            case 2:
+                cikarma = a - b;
+                System.out.println("SONUÇ: " + cikarma);
+                break;
+            case 3:
+                carpma = a * b;
+                System.out.println("SONUÇ: " + carpma);
+                break;
+            case 4:
+                bolme = a / b;
+                System.out.println("SONUÇ: " + bolme);
+                break;
+            default:
+                System.out.println("Hatalı tuşladınız...");
+                break;
+        }
+
+
+    }
+}
