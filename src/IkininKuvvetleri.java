17af69ccaf7995a93d8b05fd66d926b888e3163a
umitturkmn <umit.482@gmail.com>
2022-04-11 07:55:02 +0300

    Ödev Tamamdır

--- src/IkininKuvvetleri.java
+++ src/IkininKuvvetleri.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class İkininKuvvetleri {
+public class IkininKuvvetleri {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int a,total=0;
