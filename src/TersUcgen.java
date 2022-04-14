0570940526b248bda618514d462367df6d3142b8
umitturkmn <umit.482@gmail.com>
2022-04-14 06:05:52 +0300

    ödev tamamdır

---
+++ src/TersUcgen.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int n = input.nextInt();
+
+
+        for (int i = (n - 1); i >= 0; i--) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
