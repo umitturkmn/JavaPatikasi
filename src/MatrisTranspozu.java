eda0bcf3942081d2cef0d0eb69b83fa26073ffdf
umitturkmn <umit.482@gmail.com>
2022-04-19 18:12:18 +0300

    ödev tamam
    Co-Authored-By: umitturkmn <50139842+umitturkmn@users.noreply.github.com>

---
+++ src/MatrisTranspozu.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class MatrisTranspozu {
+    public static void main(String[] args) {
+        int[][] matrix1 = {{2,3,4},{5,6,4}};
+        int[][] matrix2 = {{1,2,3},{4,5,6}};
+
+        System.out.println(Arrays.deepToString(transpose(matrix1)));
+        System.out.println(Arrays.deepToString(transpose(matrix2)));
+    }
+
+    static int[][] transpose(int[][] arr) {
+        int[][] transposedMatrix = new int[arr[0].length][arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                transposedMatrix[j][i] = arr[i][j];
+            }
+        }
+
+        return transposedMatrix;
+    }
+}
